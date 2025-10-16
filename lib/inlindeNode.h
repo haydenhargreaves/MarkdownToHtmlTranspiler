0724ba05b38726b165d594a20055dcf72c8bb297
Preston <shultzp1@my.erau.edu>
2025-10-15 21:47:48 -0700

    Implemented WatchDog file watcher; verifies file changes and prints last modified time. All functions tested and working.

--- lib/inlindeNode.h
+++ lib/inlindeNode.h
@@ -1,17 +1,48 @@
 #ifndef INLINENODE_H
 #define INLINENODE_H
-#include "node.h"
 
-class InlineNode : public Node{
-    public:
-        InlineNode(std::string content) : content(content);
-    protected:
-        std::string content;
+#include "node.h"
+#include <string>
+
+/**
+ * @brief InlineNode class.
+ *
+ * Inherits from Node. Represents an inline object that is used
+ * by the DOM tree to convert inline tags from a markup file
+ * to an HTML file.
+ *
+ * @author Preston Shultz
+ */
+class InlineNode : public Node {
+public:
+    InlineNode(const std::string& content) : content(content) {}
+
+    // Optional: allow access to the content
+    std::string getContent() const { return content; }
+
+protected:
+    std::string content;
 };
 
-class TextNode : public InlineNode{};
-class BoldNode : public InlineNode{};
-class Italic : public InlineNode{};
-class BoldItalic : public InlineNode{};
+// These classes inherit the InlineNode constructor using `using`
+class TextNode : public InlineNode {
+public:
+    using InlineNode::InlineNode;
+};
 
-#endif 
+class BoldNode : public InlineNode {
+public:
+    using InlineNode::InlineNode;
+};
+
+class ItalicNode : public InlineNode {
+public:
+    using InlineNode::InlineNode;
+};
+
+class BoldItalicNode : public InlineNode {
+public:
+    using InlineNode::InlineNode;
+};
+
+#endif // INLINENODE_H
